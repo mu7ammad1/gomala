24bb368bf7ccbe647e8f60f494d305b34a84ee12
mu7ammad1 <153460583+mu7ammad1@users.noreply.github.com>
2026-01-22 18:56:58 +0000

    0

--- src/components/sections/countdown-timer.tsx
+++ src/components/sections/countdown-timer.tsx
@@ -86,7 +86,7 @@ export default function CountdownTimer() {
 
     useEffect(() => {
         // Target date: 2/19/2026 8:50 PM
-        const targetDate = new Date("2026-02-19T20:50:00");
+        const targetDate = new Date("2026-01-29T20:50:00");
         const targetTime = targetDate.getTime();
 
         const timer = setInterval(() => {
